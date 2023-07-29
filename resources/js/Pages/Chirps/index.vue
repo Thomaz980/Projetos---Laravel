584c954e6632cf1ac45a77e4bbd02804e1fc524d
Thomaz980 <112140581+Thomaz980@users.noreply.github.com>
2023-07-28 23:58:32 -0300

    Criando componentes Vue

---
+++ resources/js/Pages/Chirps/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import InputError from '@/Components/PrimaryButton.vue';
+import PrimaryButton from '@/Components/PrimaryButton';
+import { useForm, Head } from '@inertiajs/vue3';
+
+const form = useForm({
+    message:'',
+});
+
+</script>
+
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\ChirpController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -34,5 +36,11 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
+
+Route::resource('chirps', ChirpController::class)
+    ->only(['index', 'store'])
+    ->middleware(['auth', 'verifield']);
 
 require __DIR__.'/auth.php';
